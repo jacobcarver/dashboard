6457c7f01bac80942a085c9fa2e8ab175af09b95
Jacob Carver <jacxbcarver@gmail.com>
2018-11-01 17:05:59 -0600

    changed the layout

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -7,7 +7,7 @@ class Tasks extends Component {
             <div className="Tasks">
                 <div id="tasks">
                     <div className="header">
-                        <h1>{"Today's Tasks"}</h1>
+                        <h2>{"Your Tasks"}</h2>
                     </div>
                     <div className="content">
                         <ul className="unstyled">
@@ -16,28 +16,24 @@ class Tasks extends Component {
                                     <input className="styled-checkbox" id="styled-checkbox-1" type="checkbox" value="value1" />
                                     <label htmlFor="styled-checkbox-1">Clean office</label>
                                 </div>
-                                <i className="far fa-trash-alt"></i>
                             </li>
                             <li>
                                 <div>
                                     <input className="styled-checkbox" id="styled-checkbox-2" type="checkbox" value="value2" />
                                     <label htmlFor="styled-checkbox-2">Write thesis</label>
                                 </div>
-                                <i className="far fa-trash-alt"></i>
                             </li>
                             <li>
                                 <div>
                                     <input className="styled-checkbox" id="styled-checkbox-3" type="checkbox" value="value3" />
                                     <label htmlFor="styled-checkbox-3">Pick up supplies</label>
                                 </div>
-                                <i className="far fa-trash-alt"></i>
                             </li>
                             <li>
                                 <div>
                                     <input className="styled-checkbox" id="styled-checkbox-4" type="checkbox" value="value4" />
                                     <label htmlFor="styled-checkbox-4">Go to the gym</label>
                                 </div>
-                                <i className="far fa-trash-alt"></i>
                             </li>
                         </ul>
                     </div>
