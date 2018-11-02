8da216482a21dc9552f9d28b4d7336071122b71b
Jacob Carver <jacxbcarver@gmail.com>
2018-11-02 12:49:02 -0600

    added docs folder (build)

---
+++ docs/precache-manifest.ee26df69b29d6d66678ce5476bae921e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e27c65c7747dee0fe838",
+    "url": "/static/js/main.e27c65c7.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "e27c65c7747dee0fe838",
+    "url": "/static/css/main.24e0a831.chunk.css"
+  },
+  {
+    "revision": "456ef23d9ebb6d81147510fa64ee8b6a",
+    "url": "/index.html"
+  }
+];
